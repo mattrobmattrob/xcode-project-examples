8647414a284c5bc5370a4292104d7ebd7c6184dc
Matt Robinson <matt_rob@me.com>
2023-07-26 11:19:56 -0600

    Add iOSFramework + iOSFrameworkTests example

---
+++ 14.2-14C18/iOSAppWithTests/iOSFramework/iOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  iOSFramework.h
+//  iOSFramework
+//
+//  Created by Matt Robinson on 7/26/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSFramework.
+FOUNDATION_EXPORT double iOSFrameworkVersionNumber;
+
+//! Project version string for iOSFramework.
+FOUNDATION_EXPORT const unsigned char iOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSFramework/PublicHeader.h>
+
+
